a9d66165e467900ad942b255a8f902728e95825c
Shashwat Mittal <shashwat.mittal5@gmail.com>
2025-05-12 18:34:22 +0530

    Remove unnecessary code

--- algolia-search-interface.php
+++ algolia-search-interface.php
@@ -26,15 +26,6 @@ function algolia_search_interface_menu() {
         100
     );
 
-    // Add settings submenu
-    add_submenu_page(
-        'algolia-search-interface',
-        'Algolia Settings',
-        'Settings',
-        'manage_options',
-        'algolia-search-settings',
-        'algolia_search_settings_page'
-    );
 }
 
 // Admin page content
@@ -47,63 +38,6 @@ function algolia_search_interface_page() {
     <?php
     // Enqueue the JavaScript for the app
     algolia_enqueue_scripts();
-}
-
-// Settings page content
-function algolia_search_settings_page() {
-    if (!current_user_can('manage_options')) {
-        return;
-    }
-
-    // Save settings if form is submitted
-    if (isset($_POST['algolia_settings_nonce']) && wp_verify_nonce($_POST['algolia_settings_nonce'], 'algolia_save_settings')) {
-        $settings = array(
-            'algolia_app_id' => sanitize_text_field($_POST['algolia_app_id']),
-            'algolia_search_api_key' => sanitize_text_field($_POST['algolia_search_api_key']),
-            'algolia_admin_api_key' => sanitize_text_field($_POST['algolia_admin_api_key']),
-        );
-        update_option('algolia_search_settings', $settings);
-        echo '<div class="notice notice-success"><p>Settings saved successfully!</p></div>';
-    }
-
-    // Get current settings
-    $settings = get_option('algolia_search_settings', array(
-        'algolia_app_id' => '',
-        'algolia_search_api_key' => '',
-        'algolia_admin_api_key' => '',
-    ));
-    ?>
-    <div class="wrap">
-        <h1>Algolia Search Settings</h1>
-        <form method="post" action="">
-            <?php wp_nonce_field('algolia_save_settings', 'algolia_settings_nonce'); ?>
-            <table class="form-table">
-                <tr>
-                    <th scope="row"><label for="algolia_app_id">Application ID</label></th>
-                    <td>
-                        <input type="text" id="algolia_app_id" name="algolia_app_id" 
-                               value="<?php echo esc_attr($settings['algolia_app_id']); ?>" class="regular-text">
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row"><label for="algolia_search_api_key">Search API Key</label></th>
-                    <td>
-                        <input type="text" id="algolia_search_api_key" name="algolia_search_api_key" 
-                               value="<?php echo esc_attr($settings['algolia_search_api_key']); ?>" class="regular-text">
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row"><label for="algolia_admin_api_key">Admin API Key</label></th>
-                    <td>
-                        <input type="text" id="algolia_admin_api_key" name="algolia_admin_api_key" 
-                               value="<?php echo esc_attr($settings['algolia_admin_api_key']); ?>" class="regular-text">
-                    </td>
-                </tr>
-            </table>
-            <?php submit_button('Save Settings'); ?>
-        </form>
-    </div>
-    <?php
 }
 
 // Enqueue JavaScript and CSS
